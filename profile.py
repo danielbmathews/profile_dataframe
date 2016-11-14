b96b3281afe8a655542a91c1c178df06dd11b66d
Daniel Mathews <danielbmathews@gmail.com>
2016-11-13 19:42:39 -0800

    Fix error in counts method

--- profile.py
+++ profile.py
@@ -1,18 +1,18 @@
 import numpy as np
 import pandas as pd
 
+
 class Tests:
     """Base tests for all data types"""
 
     def __init__(self, data):
         self.data = data
         self.results = pd.DataFrame(columns=['Test', 'Result', 'Annotation'])
-        self.results2 = []
 
     def store_result(self, test, result, annotation=''):
-        self.results = self.results.append({'Test': test,
-                             'Result': result,
-                             'Annotation': annotation}, ignore_index=True)
+        self.results = self.results.append({'Test': test, 'Result': result,
+                                           'Annotation': annotation},
+                                           ignore_index=True)
         return
 
     def counts(self):
@@ -20,17 +20,26 @@ class Tests:
         count = len(self.data)
         count_distinct = len(set(self.data))
         count_missing = self.data.isnull().sum()
-        # Check for specific annotations
-        annotation = ''
+        # count
         if count == 0:
-            annotation = 'no records'
-        elif count_missing == count:
-            annotation = 'all missing'
-        elif count_distinct == 1:
-            annotation = 'all identical'
+            count_note = 'no records'
+        else:
+            count_note = ''
+        self.store_result('Count', count, count_note)
+        # count_distinct
+        if count_distinct == 1:
+            count_distinct_note = 'all identical'
         elif count_distinct == count:
-            annotation = 'all unique'
-        self.store_result('Count', count, annotation)
+            count_distinct_note = 'all unique'
+        else:
+            count_distinct_note = ''
+        self.store_result('Count distinct', count_distinct, count_distinct_note)
+        # count_missing
+        if count_missing == count:
+            count_missing_note = 'all missing'
+        else:
+            count_missing_note = 'no missing'
+        self.store_result('Count missing', count_missing, count_missing_note)
 
     def mode(self):
         """Mode"""
